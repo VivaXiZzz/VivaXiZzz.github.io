04acdebcb76903a9242153466ce11ff3774561b3
Veronichka <104949573+VivaXiZzz@users.noreply.github.com>
2023-10-27 03:24:39 +0300

    Update 7.js

--- task_7/7.js
+++ task_7/7.js
@@ -8,9 +8,6 @@ $(".responsive").slick({
       {
         breakpoint: 744,
         settings: {
-            dots: true,
-            infinite: true,
-            speed: 500,
             slidesToShow: 1,
             slidesToScroll: 1
         }
